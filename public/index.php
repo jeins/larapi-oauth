0fbee32e9c69b43ff77db50b6618c43fdfe35ede
Jeins <mail@mjuanakbar.info>
2015-04-21 15:28:55 +0200

    initialize

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MJA
+ * Date: 21.04.2015
+ * Time: 15:19
+ */
+
+use Larapi\Api\Api;
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+$api = new Api();
---
+++ src/Larapi/Api/Api.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MJA
+ * Date: 21.04.2015
+ * Time: 15:17
+ */
+
+namespace Larapi\Api;
+
+
+class Api {
+
+    public function __construct(){
+        echo "HELLO WORLD";
+    }
+} 
